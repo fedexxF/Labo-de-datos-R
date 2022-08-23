0a51e008c7d870d6e2483fbe6b0c830891dd76af
fedexxF <100316647+fedexxF@users.noreply.github.com>
2022-08-23 11:23:21 -0300

    Add files via upload

---
+++ Clase Practica 23-08.R
@@ -0,0 +1,40 @@
+#Guia 1 Labo de datos
+#23/08
+#
+
+#------------ej1----------
+#Ejecutar e interpretar el resultado del siguiente conjunto de sentencias:
+#Todos ocupan 64 bits, son de doble precision
+
+
+#Es floating mas pequeño positivo que reulta de 1+x!=1
+#La dsitancia, epsilon, desde el 1 hasta el siguinete numero de precision doble
+
+.Machine$double.eps
+
+#el epsilon tal que 1-x!=1
+
+.Machine$double.neg.eps
+
+# el numero de punto flotante mas pequeño que puede representar la maquina
+
+.Machine$double.xmin
+
+#el numero de punto flotante mas grande que puede representar la maquina
+
+.Machine$double.xmax
+
+#Es la base para la cual se representa el punto flotante
+
+.Machine$double.base
+
+#la cantidad de digitos en el significado del numero del punto flotante (la mantisa)
+
+.Machine$double.digits
+
+#El entero maximo que puede representar la maaquina
+
+.Machine$integer.max
+
+
+
