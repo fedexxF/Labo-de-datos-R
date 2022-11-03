9e7c0c27e36aaac5f11bdb19e460902cbe4b0bcc
Federico Florio <100316647+fedexxF@users.noreply.github.com>
2022-11-02 23:35:36 -0300

    Add files via upload

--- Practicas/Practica 5/Practica 5.R
+++ Practicas/Practica 5/Practica 5.R
@@ -128,9 +128,6 @@ ggsave("salidas/SAM.jpg",dpi=600, units="cm", height=15,width=30)
 #SON
 
 DEF<-c(tabla1[,1],tabla1[,2],tabla1[,12])
-MAM<-tabla1[]
-JJA<-tabla1[]
-SON<-tabla1[]
 
 # Genero un array de 66x2 vacío donde guardar los datos
 Tabla_Completar_DEF <- array(NA, c(66,2))
@@ -159,4 +156,93 @@ fechas
 df_DEF <- data.frame(fecha=fechas, SAM=SAM_PROM_DEF)
 df_DEF
 
+DEF<-cbind(tabla1[,1],tabla1[,2],tabla1[,12])
+MAM<-cbind(tabla1[,3],tabla1[,4],tabla1[,5])
+MAM
+JJA<-cbind(tabla1[,6],tabla1[,7],tabla1[,8])
+SON<-cbind(tabla1[,9],tabla1[,10],tabla1[,11])
 
+DEF_PROM<-rowMeans(DEF,na.rm = TRUE)
+MAM_PROM<-rowMeans(MAM,na.rm = TRUE)
+MAM_PROM
+JJA_PROM<-rowMeans(JJA,na.rm = TRUE)
+SON_PROM<-rowMeans(SON,na.rm = TRUE)
+
+
+Estacion_PROM<-cbind(DEF_PROM,MAM_PROM,JJA_PROM,SON_PROM)
+Estacion_PROM
+
+
+
+# Genero un array de 66x4 vacío donde guardar los datos
+#Tabla_Completar <- array(NA, c(66,4))
+#Tabla_Completar
+#Le pongo nombre a las columnas
+#colnames(Tabla_Completar) <- colnames(tabla1)
+
+#Recorro las columnas de la tabla original,les guardo los datos que cargamos en la tabla
+
+#for(i in 1:4){
+#  Tabla_Completar[,i] <- Estacion_PROM[1:66,i]  #a tabla de 1 a 7 y de columnas i (1 a 12). Guardalas por columnas en Tabla_Completar
+#}
+#Tabla_Completar
+
+#Eje y: SAM
+#SAM<- as.vector(t(Tabla_Completar)) # t traspone la matriz
+#SAM
+
+#Creo el df
+#df <- data.frame(fecha=fechas, SAM=SAM)
+#df
+
+
+# 2) Capa de datos y mapeo de estéticas -----------------------------------
+
+#Llamo a la función ggplot para comenzar un gráfico
+#Primer parametro: dataframe
+#Segundo parametro: mapeo de esteticas con aes()
+g <- ggplot(df, aes(x=fechas,y=SAM))
+g
+
+# 3) Capa geometrica ------------------------------------------------------
+
+# En este caso quiero hacer un grafico de barras, la capa correcta es
+# geom_col()
+g <- g + geom_col(aes(fill=SAM)) #usamos fill porque fill es de relleno y colors es de relieve
+g
+
+#Me gustaria remarcar el eje donde y=0. Agrego la capa geométrica
+#geom_hline()
+g <- g + geom_hline(yintercept=0) #geom_hline agrega una recta horizontal
+g
+
+# 4) Capas extra para personalizar el gráfico -----------------------------
+
+#Etiquetas
+g <- g + labs(title="Evolucion temporal del SAM",
+              subtitle="Respecto del período 1957-2022",
+              x="años", #Etiquetas, podria ser tiempo
+              y="SAM [No se las unidades ]",
+              fill="No se las unidades") #Etiquetas, podria agregarla
+g
+
+#Intervalos del eje y
+g <- g + scale_y_continuous(breaks=seq(-6,4,1)) 
+g
+
+#Intervalos de tiempo del eje x
+g <- g +scale_x_date(breaks="4 year",date_labels="%Y")
+g
+
+#Colores ed las barras
+g <- g + scale_fill_manual(values=c("#E06F21","#00A89D"))
+g
+
+
+
+# 5) Guardamos el gráfico -------------------------------------------------
+
+ggsave("salidas/SAM.jpg",dpi=600, units="cm", height=15,width=30)
+
+#c) Testear si el  ́ındice presenta una tendencia significativa. Filtrar dicha tendencia y
+#graficar ambas series en una misma figura.
