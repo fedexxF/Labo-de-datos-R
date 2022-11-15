383c0ba74326cc21fb679e5fda958095605ca6b0
Federico Florio <100316647+fedexxF@users.noreply.github.com>
2022-11-15 12:57:50 -0300

    Add files via upload

--- TP FINAL/TP Final Federico Florio 343 20 V4.R
+++ TP FINAL/TP Final Federico Florio 343 20 V4.R
@@ -1,76 +1,36 @@
+##################################################################################################################
+#Instalamos las librerias a usar
 
-library(microbenchmark)
+install.packages("metR")
+install.packages("ggplot2")
+install.packages("maps")
 
-for1<-function(x){
-  for (i in 1:length(x)) {
-    datos_array<-array(
-      readBin(paste0(datos,archivos[i]),"numeric", size=4, n=nrecords, endian="little"),
-      dim=c(nlons,nlats,nlevs,nvars,ntime)
-    )
-  }
-}  
-
-for2<-function(x){
-  datos_array<-array(NA,dim=c(nlons,nlats,nlevs,nvars,ntime))
-  for (i in 1:length(x)) {
-    datos_array<-array(
-      readBin(paste0(datos,archivos[i]),"numeric", size=4, n=nrecords, endian="little"),
-      dim=c(nlons,nlats,nlevs,nvars,ntime)
-    )
-  }
-}  
-
-x<-8
-microbenchmark(for1(x),for2(x))
-##################################################################################################################################
-
-profvis({
-  
-  #Tenemos que fijar la presion a 850 hpa y tomar las variables 2 y 3 (viento zonal y meridional)  
-  
-  
-  Viento_zonal_850<-datos_array[,,which(levels==850),2,]
-  Viento_zonal_600<--datos_array[,,which(levels==600),2,]
-  Viento_meridional_850<-datos_array[,,which(levels==850),3,]
-  Viento_meridional_600<-datos_array[,,which(levels==600),3,]
-  
-  #Debemos usar el viento en modulo, es decir, 
-  #v=sqrt(u^2+v^2) 
-  
-  Viento_850<-sqrt((Viento_zonal_850)^2+(Viento_meridional_850)^2)
-  Viento_600<-sqrt((Viento_zonal_600)^2+(Viento_meridional_600)^2)
-  
-
-#Tenemos que fijar la presion a 850 hpa y tomar las variables 2 y 3 (viento zonal y meridional)  
-
-
-Viento_zonal_850<-Viento_zonal_1000_500[,,which(levels==850),]
-Viento_zonal_600<-Viento_zonal_1000_500[,,which(levels==600),]
-Viento_meridional_850<-Viento_meridional_1000_500[,,which(levels==850),]
-Viento_meridional_600<-Viento_meridional_1000_500[,,which(levels==600),]
-
-#Debemos usar el viento en modulo, es decir, 
-#v=sqrt(u^2+v^2) 
-
-Viento_850<-sqrt((Viento_zonal_850)^2+(Viento_meridional_850)^2)
-Viento_600<-sqrt((Viento_zonal_600)^2+(Viento_meridional_600)^2)
-  
-})
-
-
-
-
-##################################################################################################################################
-
-library(profvis)
-library(data.tree)
 # Cargamos librerias
 library(ggplot2)
 library(metR)
 library(maps)
 
-profvis({
-  
+#################################################################################################################
+#########################################ENUNCIADO###############################################################
+
+# Corriente en chorro en capas bajas (LLJ)
+#Para estimar la posición del LLJ en el norte argentino se puede utilizar el criterio de
+#Bonner, donde la condición para que se cumpla el mismo es que el viento en el nivel
+#elegido sea superior a 12 m/s y la cortante vertical del viento entre ese nivel y un nivel
+#superior supere los 6 m/s. Se tienen los datos de análisis del GDAS, de un evento de
+#LLJ, en formato binario
+#a) Leer los datos del viento zonal y meridional para los niveles entre 1000 y 500 hPa.
+#b) Calcular el criterio de Bonner en el nivel de 850 hPa para todos los tiempos (nivel superior 600 hPa).
+#c) Definir una variable donde su valor es 1 si se cumple el criterio de bonner.
+#d) Graficar en 4 paneles la evolución del criterio de Bonner junto con el viento en
+#vectores.
+#e) Guardar la variable del criterio de Bonner en un archivo binario de doble precisión y
+#Little Endian. (Para los puntos donde no se cumple el criterio definir un valor de undef). Crear un archivo de control (header).
+
+
+#################################################################################################################
+#################################################################################################################
+
 #Borramos cualqueir variable cargada en R y los graficos/plots
 
 rm(list=ls())
@@ -78,9 +38,9 @@ graphics.off()
 
 #Seteamos directorios de trabajo (facultad)
 
-#setwd("~/Escritorio/LABO/Labo-de-datos-R-main/TP FINAL")
-#datos <- "~/Escritorio/LABO/Labo-de-datos-R-main/TP FINAL/datos/"
-#salidas <- "~/Escritorio/LABO/Labo-de-datos-R-main/TP FINAL/salidas/"
+setwd("~/Escritorio/LABO/Labo-de-datos-R-main/TP FINAL")
+datos <- "~/Escritorio/LABO/Labo-de-datos-R-main/TP FINAL/datos/"
+salidas <- "~/Escritorio/LABO/Labo-de-datos-R-main/TP FINAL/salidas/"
 
 #Seteamos directorios de trabajo (Casa)
 
@@ -127,13 +87,22 @@ archivos<-list.files(".//datos", pattern = "\\.gra")
 
 
 #Con ciclo for abrimos todos los archivos 
-
+#lista=list()
+vector=c()
 for (i in 1:length(archivos)) {
-  datos_array<-array(
-    readBin(paste0(datos,archivos[i]),"numeric", size=4, n=nrecords, endian="little"),
-    dim=c(nlons,nlats,nlevs,nvars,ntime)
-  )
+  print(paste0(datos,archivos[i]))
+  vector = c(vector, readBin(paste0(datos,archivos[i]),"numeric", size=4, n=nrecords, endian="little"))
+  print(length(vector))
+  #lista[[i]]<-array(
+  #  readBin(paste0(datos,archivos[i]),"numeric", size=4, n=nrecords, endian="little"),
+  #  dim=c(nlons,nlats,nlevs,nvars)
+  #)
+  
 }
+
+datos_array<-array(vector,
+  dim=c(nlons,nlats,nlevs,nvars,ntime)
+)
 
 #Sera mejor hacer esto?:
 #Leer_Binarios<-function(archivo){
@@ -172,6 +141,7 @@ Viento_zonal_600<-Viento_zonal_1000_500[,,which(levels==600),]
 Viento_meridional_850<-Viento_meridional_1000_500[,,which(levels==850),]
 Viento_meridional_600<-Viento_meridional_1000_500[,,which(levels==600),]
 
+
 #Debemos usar el viento en modulo, es decir, 
 #v=sqrt(u^2+v^2) 
 
@@ -189,147 +159,6 @@ Viento_600<-sqrt((Viento_zonal_600)^2+(Viento_meridional_600)^2)
 Cortante_Viento<-abs(Viento_600-Viento_850)
 
 ################################################################################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-################################################################################################################
-profvis({
-  
-  #Borramos cualqueir variable cargada en R y los graficos/plots
-  
-  rm(list=ls())
-  graphics.off()
-  
-  #Seteamos directorios de trabajo (facultad)
-  
-  #setwd("~/Escritorio/LABO/Labo-de-datos-R-main/TP FINAL")
-  #datos <- "~/Escritorio/LABO/Labo-de-datos-R-main/TP FINAL/datos/"
-  #salidas <- "~/Escritorio/LABO/Labo-de-datos-R-main/TP FINAL/salidas/"
-  
-  #Seteamos directorios de trabajo (Casa)
-  
-  setwd("D:/Users/Windows 10/Desktop/LABO/Labo-de-datos-R-main/TP FINAL")
-  datos <- "D:/Users/Windows 10/Desktop/LABO/Labo-de-datos-R-main/TP FINAL/datos/"
-  salidas <- "D:/Users/Windows 10/Desktop/LABO/Labo-de-datos-R-main/TP FINAL/salidas/"
-  
-  # Leemos el archivo "hbonner.ctl" y extraemos las variables
-  
-  #dset ^bonner200703%d2%h2.gra
-  #undef 9.999E+20
-  #title Bajas segregadas
-  #options little_endian
-  #options template
-  #ydef 71 linear -70.000000 1
-  #xdef 141 linear 200.000000 1.000000
-  #tdef 8 linear 18Z26mar2007 6hr
-  #zdef 21 levels 1000 975 950 925 900 850 800 750 700 650 600 550 500 450 400 350 300 250 200 150 100
-  #vars 4
-  #tt  21 99 temperatura [K]
-  #u   21 99 viento zonal [m/s]
-  #v   21 99 viento meridional [m/s]
-  #hgt 21 99 Geopotencial [mgp]
-  #endvars
-  
-  
-  #Extraemos los datos:
-  nlats<-71
-  nlons<-141
-  nlevs<-21
-  nvars<-4
-  ntime<-8
-  
-  #Varriables totales
-  
-  nrecords<-nlevs*nvars*nlats*nlons
-  
-  
-  #Con list.files le pedimos que lea los archivos que se encuentran en la carpeta /datos
-  #Con pattern le pedimos que carge solo los archivos con extension .gra
-  #Guardamos en una varible los archivos a leer
-  
-  archivos<-list.files(".//datos", pattern = "\\.gra")
-  
-  
-  #Con ciclo for abrimos todos los archivos 
-  
-  vector=c()
-  for (i in 1:length(archivos)) {
-    print(paste0(datos,archivos[i]))
-    vector = c(vector, readBin(paste0(datos,archivos[i]),"numeric", size=4, n=nrecords, endian="little"))
-    print(length(vector))
-    #lista[[i]]<-array(
-    #  readBin(paste0(datos,archivos[i]),"numeric", size=4, n=nrecords, endian="little"),
-    #  dim=c(nlons,nlats,nlevs,nvars)
-    #)
-    
-  }
-  
-  datos_array<-array(vector,
-                     dim=c(nlons,nlats,nlevs,nvars,ntime)
-  )
-  
-  
-  
-  
-  for (i in 1:length(archivos)) {
-    datos_array<-array(
-      readBin(paste0(datos,archivos[i]),"numeric", size=4, n=nrecords, endian="little"),
-      dim=c(nlons,nlats,nlevs,nvars,ntime)
-    )
-  }
-  
-  abrir_binario<- function(x){
-    datoss<-readBin(x,"numeric", size=4, n=nrecords, endian="little")
-    data<-array(datoss,dim=c(nlons,nlats,nlevs,nvars))
-    return(data)
-  }
-  
-  setwd("D:/Users/Windows 10/Desktop/LABO/Labo-de-datos-R-main/TP FINAL/datos")
-  
-  array26_12<-abrir_binario("D:/Users/Windows 10/Desktop/LABO/Labo-de-datos-R-main/TP FINAL/datos/bonner2007032612.gra")
-  array26_18<-abrir_binario("D:/Users/Windows 10/Desktop/LABO/Labo-de-datos-R-main/TP FINAL/datos/bonner2007032618.gra")
-  array27_00<-abrir_binario("D:/Users/Windows 10/Desktop/LABO/Labo-de-datos-R-main/TP FINAL/datos/bonner2007032700.gra")
-  array27_06<-abrir_binario("D:/Users/Windows 10/Desktop/LABO/Labo-de-datos-R-main/TP FINAL/datos/bonner2007032706.gra")
-  array27_12<-abrir_binario("D:/Users/Windows 10/Desktop/LABO/Labo-de-datos-R-main/TP FINAL/datos/bonner2007032712.gra")
-  array27_18<-abrir_binario("D:/Users/Windows 10/Desktop/LABO/Labo-de-datos-R-main/TP FINAL/datos/bonner2007032718.gra")
-  array28_00<-abrir_binario("D:/Users/Windows 10/Desktop/LABO/Labo-de-datos-R-main/TP FINAL/datos/bonner2007032800.gra")
-  array28_06<-abrir_binario("D:/Users/Windows 10/Desktop/LABO/Labo-de-datos-R-main/TP FINAL/datos/bonner2007032806.gra")
-
-
-array_tiempos<-array(c(array26_12,array26_18,array27_00,array27_06,array27_12,array27_18,array28_00,array28_06),c(nlons,nlats,nlevs,nvars,ntime))
-
-})
-
-
-###################################################################################################################
 
 #Necesito los valores TRUE o FALSE en los puntos de reticula en donde se cumple el criterio de bonner
 #Teniamos dos variables que contenian viento zonal y Cortante de viento
@@ -356,8 +185,8 @@ Criterio_de_Bonner_TRUE[[TRUE]]<-1
 lon <- seq(from=200.00,by=1,length.out=nlons)   #Tambien sacado del CTL
 lats <- seq(from=-70.00, by=1, length.out=nlats)
 
-x <- rep(lon,nlats) #Se va a repetir 71 veces
-y <- rep(lats, each=nlons) #Se va a repetir 141 veces
+x <- rep(rep(lon,nlats),4) #Se va a repetir 71 veces
+y <- rep(rep(lats, each=nlons),4) #Se va a repetir 141 veces
 
 x
 y
@@ -369,6 +198,8 @@ length(y)
 
 df <- data.frame(lon=x,lats=y)
 df
+
+
 
 #pero esto mismo lo hace metR <3 :
 x_y <- expand.grid(x = lon, y = lats) 
@@ -382,14 +213,27 @@ df==df1
 
 #Grafiquemos el viento: para eso tenemos que agregar a u y a v a nuestro df:
 
-Viento_zonal_850_12<-datos_array[,,levels==850,2,2]
-Viento_meridional_850_12<-datos_array[,,levels==850,3,2]
+Viento_zonal_850_12<-c(Viento_zonal_850[,,2],Viento_zonal_850[,,4],Viento_zonal_850[,,6],Viento_zonal_850[,,8])
+Viento_meridional_850_12<-c(Viento_meridional_850[,,2],Viento_meridional_850[,,4],Viento_meridional_850[,,6],Viento_meridional_850[,,8])
 
 df$Viento_zonal_850_12 = as.vector(Viento_zonal_850_12)  #Lo guardamos en el data frame como vector
 df$Viento_meridional_850_12 = as.vector(Viento_meridional_850_12)  #Lo guardamos en el data frame en la columna V
 
+Datos_12_hs<-c(rep("26/03 12hs",141*71),rep("27/03 00hs",141*71),rep("27/03 12hs",141*71),rep("28/03 00hs",141*71))
+df$Datos_Cada_12_hs<-as.vector(Datos_12_hs)
 
-mapa <- map_data("world") #Cargamos un mapa
+df[[10012,3]]
+df[[10012,4]]
+df[[10013,3]]
+df[[10013,4]]
+df[[10014,3]]
+df[[10014,4]]
+
+sum(df[1:(141*71),3] != df[(141*71)+1:(141*71*2),3])
+
+
+
+mapa <-map_data("world") #Cargamos un mapa2
 
 #Devuelve un dataframe listo para graficar un mapa. 
 #Podemos usar geom_path() o geom_polygon() en caso de querer colorear los contientes
@@ -427,7 +271,7 @@ df$lon <- ConvertLongitude(df$lon)
 ggplot(df,aes(x=lon,y=lats))+
   geom_arrow(aes(dx=Viento_zonal_850_12, dy=Viento_meridional_850_12), 
              skip = 2,           
-             size = 0.01,
+             size = 0.3,
              arrow.type = "open",
              color = "black")+
   coord_quickmap(xlim = range(df$lon), ylim = range(df$lats), expand = FALSE)+
@@ -443,28 +287,32 @@ ggplot(df,aes(x=lon,y=lats))+
 
 #Grafiquemos Criterio de Bonner del dia 2
 
-Criterio_de_Bonner_TRUE8500<-Criterio_de_Bonner_TRUE[,,2]
+Criterio_de_Bonner_Evaluacion<-c(Criterio_de_Bonner_TRUE[,,2],Criterio_de_Bonner_TRUE[,,4],Criterio_de_Bonner_TRUE[,,6],Criterio_de_Bonner_TRUE[,,8])
 
-df$Criterio_de_Bonner_TRUE8500 = as.vector(Criterio_de_Bonner_TRUE8500)  #Lo guardamos en el data frame como vector
+df$Criterio_de_Bonner_Evaluacion = as.vector(Criterio_de_Bonner_Evaluacion)  #Lo guardamos en el data frame como vector
 
 #grafico 1
 
 ggplot(df,aes(x=lon,y=lats))+
-  geom_tile(aes(fill=Criterio_de_Bonner_TRUE8500))+
+  geom_tile(aes(group=Criterio_de_Bonner_Evaluacion,fill=factor(Criterio_de_Bonner_Evaluacion)))+
   coord_quickmap(xlim = range(df$lon), ylim = range(df$lats), expand = FALSE)+
   mi_mapa+
   labs(title="Criterio de Bonner 850 hPa",
        x = "Longitud", 
        y = "Latitud",
        fill = "[Criterio Bonner]")+
-  scale_fill_distiller(palette = "Greys", direction = 1)
-
+  scale_discrete_manual(
+    values = c("white","grey") ,
+    aesthetics = "fill",
+    breaks = c(0,1),
+    labels = c("No cumple", "Cumple")
+  )
 
 # 6) Unimos los mapas -----------------------------------------------------
 
 #Unamos ambos mapas:
-ggplot(df,aes(x=lon,y=lats))+
-  geom_tile(aes(fill=Criterio_de_Bonner_TRUE8500))+
+Grafico_mapa<-ggplot(df,aes(x=lon,y=lats))+
+  geom_tile(aes(group=Criterio_de_Bonner_Evaluacion,fill=factor(Criterio_de_Bonner_Evaluacion)))+
   mi_mapa+
   geom_arrow(aes(dx=Viento_zonal_850_12, dy=Viento_meridional_850_12), 
              skip = 2,           
@@ -477,19 +325,38 @@ ggplot(df,aes(x=lon,y=lats))+
   labs(title="Viento y Criterio de Bonner 850 hPa",
        x = "Longitud", 
        y = "Latitud",
-       fill = "[Criterio Bonner]")+
-  scale_fill_distiller(palette = "Greys", direction = 1)+ 
+       fill = "Criterio Bonner")+
+  scale_discrete_manual(
+    values = c("white","grey") ,
+    aesthetics = "fill",
+    breaks = c(0,1),
+    labels = c("No cumple", "Cumple")
+  )+ 
   scale_y_continuous(breaks=seq(-90,90,10))+    #Cambio de escalas en y
   scale_x_continuous(breaks=seq(-180,180,30))+  #Cambio de escalas en x
   theme_bw()
+  
+Grafico_mapa + facet_wrap( ~ Datos_Cada_12_hs, ncol=2)+
+  theme(strip.text.x = element_text(size=8, angle=0),
+        strip.background = element_rect(colour="black", fill="#CCCCFF"))
 
 
-})
+#e) Guardar la variable del criterio de Bonner en un archivo binario de doble precisión y
+#Little Endian. (Para los puntos donde no se cumple el criterio definir un valor de undef). Crear un archivo de control (header).
+
+#ejemplo de uso de readBin writeBin
+
+#1 Escribo y leo los contenidos de un archivo:
+
+#creo una matriz en el workspace!
+Criterio_de_Bonner_TRUE_NA=as.vector(Criterio_de_Bonner)
 
 
+#Creo el archivo
+fid = file('CriterioBonner.bin', 'wb') #Permisos para escribir
+fid    
+writeBin(Criterio_de_Bonner_TRUE_NA, fid) #guardo a en el archivo fid
+close(fid)       #Lo cierro. ya esta
 
-
-
-
-
+ArchivO_CTL<-write("")
 
